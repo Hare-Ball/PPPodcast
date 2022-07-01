7bb3aa7a83fda17274bbb36d486e7daea15f22d5
Hare-Ball <robby.hare@gmail.com>
2022-07-01 12:52:41 -0500

    updated header text

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import './Header.css'
+
+function Header() {
+    return (
+        <header className="container col-2 justify-content-center" id="header">
+            <h1>Welcome to the Preaching Poetry Podcast</h1>
+        </header>
+ )}       
+export default Header;
---
+++ src/components/Project.js
@@ -0,0 +1,33 @@
+// import React, {useState} from 'react';
+// import Footer from './Footer';
+import Header from './Header';
+// import Navbar from './Navbar';
+
+function Project() {
+    // const [currentPage, setCurrentPage] = useState('AboutMe');
+
+    // const renderPage = () => {
+    //     if (currentPage === 'AboutMe') {
+    //         return <AboutMe />;
+    //     }
+    //     if (currentPage === 'ContactInfo') {
+    //         return <ContactMe />;
+    //     }
+    //     if (currentPage === 'Portfolio') {
+    //         return <Portfolio />;
+    //     }
+    //     if (currentPage === 'Resume') {
+    //         return <Resume />;
+    //     }
+    //     }
+
+    // const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div id="Project">
+          <Header />
+        </div>
+      );
+    }   
+      
+export default Project;
