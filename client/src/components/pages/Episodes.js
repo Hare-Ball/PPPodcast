94332be073c5596d613c53965ef4f6ef5823e9b7
Hare-Ball <robby.hare@gmail.com>
2022-07-18 11:46:05 -0500

    added React Player to embed Youtube playlist

--- client/src/components/pages/Episodes.js
+++ client/src/components/pages/Episodes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import './AboutMe.css'
 import ytbanner from '../../assets/images/Alt YT Banner.png'
+import ReactPlayer from 'react-player/youtube'
 
 
 function AboutUs() {
@@ -9,9 +10,14 @@ function AboutUs() {
            <section class="card">
            <img class="container col-9 justify-content-center" alt= "Video Banner" src={ytbanner}></img>
                     <div class="container col-7 justify-content-center" id= "AboutMeText" >
-                        <p>Watch Episodes here or on YouTube</p>               
+                        <p>You can subscribe to the podcast to listen to the episodes, or you can listen to them here on YouTube</p>               
                     </div>
             </section>
+            <br></br>
+            <section class="container col-12 justify-content-center" alt="Youtube Playlist">
+                <ReactPlayer url="https://www.youtube.com/watch?v=-R0Z3BoGvPg&list=PL5jaVg6C7wiZ0UDGwE1ouxr7eqRGO5iJU" />
+            </section>
+            <br></br>    
         </div>
     )
 }
