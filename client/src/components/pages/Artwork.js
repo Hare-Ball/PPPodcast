1edf2ce771c6b3d1ebab9b22128c0194cab7209d
Hare-Ball <robby.hare@gmail.com>
2022-09-10 18:56:53 -0500

    updates to youtube display and to photo gallery UI

--- client/src/components/pages/Artwork.js
+++ client/src/components/pages/Artwork.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import './Artwork.css'
+import './Artwork.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
 import opportunity1 from '../../assets/images/Season 1 Art/Opportunity- Edward R. Sill 1.png';
 import opportunity2 from '../../assets/images/Season 1 Art/Opportunity- Edward R. Sill 2.png';
 import opportunity3 from '../../assets/images/Season 1 Art/Opportunity- Edward R. Sill 3.png';
@@ -16,15 +14,15 @@ function Artwork() {
   return (
     <Container>
       <Row>
-        <Col> <img id= "instagram" alt= "Opportunity Artwork 1" src={opportunity1}></img></Col>
-        <Col> <img id= "instagram" alt= "Opportunity Artwork 2" src={opportunity2}></img></Col>
-        <Col> <img id= "instagram" alt= "Opportunity Artwork 3" src={opportunity3}></img></Col>
+         <img id= "instagram" alt= "Opportunity Artwork 1" src={opportunity1}></img>
+         <img id= "instagram" alt= "Opportunity Artwork 2" src={opportunity2}></img>
+         <img id= "instagram" alt= "Opportunity Artwork 3" src={opportunity3}></img>
       </Row>
       <Row>
-        <Col> <img id= "instagram" alt= "Invictus Artwork 1" src={invictus1}></img></Col>
-        <Col> <img id= "instagram" alt= "Invictus Artwork 2" src={invictus2}></img></Col>
-        <Col> <img id= "instagram" alt= "Invictus Artwork 3" src={invictus3}></img></Col>
-        <Col> <img id= "instagram" alt= "Invictus Artwork 4" src={invictus4}></img></Col>
+        <img id= "instagram" alt= "Invictus Artwork 1" src={invictus1}></img>
+        <img id= "instagram" alt= "Invictus Artwork 2" src={invictus2}></img>
+        <img id= "instagram" alt= "Invictus Artwork 3" src={invictus3}></img>
+        <img id= "instagram" alt= "Invictus Artwork 4" src={invictus4}></img>
       </Row>
     </Container>
   );
--- client/src/components/pages/Episodes.js
+++ client/src/components/pages/Episodes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Episodes.css'
-import ytbanner from '../../assets/images/Alt YT Banner.png'
+// import ytbanner from '../../assets/images/Alt YT Banner.png'
 import ReactPlayer from 'react-player/youtube'
 
 
@@ -8,16 +8,19 @@ function AboutUs() {
     return(
         <div class="container col-12 justify-content-center" id= "AboutMe">
            <section class="card container col-12 justify-content-center">
-           <img class="container col-12 justify-content-center" alt= "Video Banner" src={ytbanner}></img>
+           {/* <img class="container col-12 justify-content-center" alt= "Video Banner" src={ytbanner}></img> */}
                     <div class="container col-9 justify-content-center" id= "AboutMeText" >
+                        <br></br>
                         <h3>Subscribe to the podcast to listen to the episodes, or you can listen to them here on YouTube</h3> 
                         <br></br>              
                     </div>
             </section>
             <br></br>
-            <section class="container col-6 justify-content-center" alt="Youtube Playlist" id="videoPlayer">
-                <ReactPlayer url="https://www.youtube.com/watch?v=-R0Z3BoGvPg&list=PL5jaVg6C7wiZ0UDGwE1ouxr7eqRGO5iJU" />
-            </section>
+            <center>
+                <section class="container col-12 justify-content-center" alt="Youtube Playlist" id="videoPlayer">
+                    <ReactPlayer url="https://www.youtube.com/watch?v=-R0Z3BoGvPg&list=PL5jaVg6C7wiZ0UDGwE1ouxr7eqRGO5iJU" />
+                </section>
+            </center>
             <br></br>    
         </div>
     )
